b09e5e816ef72ab96cdcc928711c6bbfa6be00f4
ikmrn <ibragimovkmrn@gmail.com>
2024-08-03 13:50:28 +0500

    feat: Add multiple utility packages

--- global_config.sh
+++ global_config.sh
@@ -17,9 +17,9 @@ AUR_HELPERS=("paru" "yay")
 #######################
 ### Pacman Packages ###
 ########################
-PACKAGES=("vim" "neovim" "firefox" "fastfetch" "btop" "stow" "tldr" "man")
+PACKAGES=("vim" "neovim" "firefox" "fastfetch" "btop" "stow" "tldr" "man" "grim" "slurp" "cliphist" "waybar" "mako" "polkit-gnome" "pamixer" "pavucontrol" "brightnessctl" "bluez" "bluez-utils" "blueman" "network-manager-applet" "gvfs" "thunar-archive-plugin" "file-roller" "starship" "lxappearance" "xfce4-settings pipewire pipewire-pulse pipewire-alsa pipewire-jack wireplumber") 
 
 ####################
 ### AUR Packages ###
 ####################
-AUR_PACKAGES=("visual-studio-code-bin" "ttf-ms-fonts" "hyrpland" "hyprpaper")
+AUR_PACKAGES=("visual-studio-code-bin" "ttf-ms-fonts" "hyrpland" "hyprpaper" "rofi-lbonn-wayland-git" "wlogout-git" "xdg-desktop-portal-hyprland-git")
