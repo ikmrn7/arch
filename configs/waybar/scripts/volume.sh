c81f8210a3f91aa7f9b239361ed43df884e224fa
ikmrn <ibragimovkmrn@gmail.com>
2024-08-08 15:15:01 +0500

    feat: Add unmute functionality

--- configs/waybar/scripts/volume.sh
+++ configs/waybar/scripts/volume.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 STEP=5
-
+# FIX unmute if volume is increased or descreased
 if [ "$1" == "up" ]; then
+  pactl set-sink-mute @DEFAULT_SINK@ 0
   pactl set-sink-volume @DEFAULT_SINK@ +${STEP}%
 elif [ "$1" == "down" ]; then
+  pactl set-sink-mute @DEFAULT_SINK@ 0
   pactl set-sink-volume @DEFAULT_SINK@ -${STEP}%
 fi
