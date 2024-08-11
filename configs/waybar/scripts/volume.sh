9e5d4b339d592ece203628c4b41942bb65e0b356
ikmrn <ibragimovkmnr@gmail.com>
2024-08-11 13:35:34 +0500

    feat: Add sound when volume changes

--- configs/waybar/scripts/volume.sh
+++ configs/waybar/scripts/volume.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
 STEP=5
-# FIX unmute if volume is increased or descreased
+LIMIT=200
+SINK=@DEFAULT_SINK@
+
+# Get current volume level
+current_volume=$(pactl get-sink-volume $SINK | awk '{print $5}' | sed 's/%//')
+
+# Adjust volume
 if [ "$1" == "up" ]; then
-  pactl set-sink-mute @DEFAULT_SINK@ 0
-  pactl set-sink-volume @DEFAULT_SINK@ +${STEP}%
+    pactl set-sink-mute $SINK 0
+  # Check if current volume is less than the limit
+  if [ "$current_volume" -lt "$LIMIT" ]; then
+    pactl set-sink-volume $SINK +${STEP}%
+  fi
 elif [ "$1" == "down" ]; then
-  pactl set-sink-mute @DEFAULT_SINK@ 0
-  pactl set-sink-volume @DEFAULT_SINK@ -${STEP}%
+  pactl set-sink-mute $SINK 0
+  pactl set-sink-volume $SINK -${STEP}%
 fi
+
+# Play the sound
+mpv --no-video ~/.config/waybar/scripts/pop.mp3
