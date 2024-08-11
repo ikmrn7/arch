cabb25b2355826538383ea7ff611bd6c3f7f2f70
ikmrn <ibragimovkmnr@gmail.com>
2024-08-11 13:49:04 +0500

    refactor: Move SDDM theme config to services_and_themes.sh

--- services_and_themes.sh
+++ services_and_themes.sh
@@ -4,8 +4,22 @@ sudo systemctl enable bluetooth.service
 sudo systemctl enable sddm.service
 
 echo "Applying themes"
-kvantummanager --set LayanDark
 papirus-folders --theme Papirus-Dark -C cat-mocha-teal
-# Apply themes
 
-./install_sddm_theme.sh
+# Apply sddm theme
+#!/bin/bash
+
+THEME="/usr/share/sddm/themes/sddm-astronaut-theme"
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+sudo git clone https://github.com/keyitdev/sddm-astronaut-theme.git "$THEME"
+sudo cp /usr/share/sddm/themes/sddm-astronaut-theme/Fonts/* /usr/share/fonts/
+
+sudo cp -r "$script_dir/configs/wallpapers" "$THEME/wallpapers/"
+echo "[Theme]
+Current=sddm-astronaut-theme" | sudo tee /etc/sddm.conf
+
+sudo sed -i 's/FormPosition="center"/FormPosition="right"/' "$THEME/theme.conf"
+
+# Use sed to replace the Background line with the new wallpaper path
+sudo sed -i 's|^Background=.*|Background="wallpapers/ox.jpg"|' "$THEME/theme.conf"
