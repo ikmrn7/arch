caa5cc248a3226477141d99589434111489d576a
ikmrn <ibragimovkmrn@gmail.com>
2024-08-13 00:12:43 +0500

    chore: Improve notify messages for clarity

--- configs/waybar/scripts/update.sh
+++ configs/waybar/scripts/update.sh
@@ -2,20 +2,21 @@
 
 # Notify the user that the system update is starting
 notify-send "System Update" "Updating your system."
+sleep 1
 
 # Update the system using pacman and AUR packages
 if command -v pacman &> /dev/null; then
-    echo "Updating system with pacman..."
+    notify-send "System Update" "Updating system with pacman..."
     sudo pacman -Syu --noconfirm
 
 elif command -v paru &> /dev/null; then
     notify-send "System Update" "Updating AUR packages."
-    sudo paru -Syu --noconfirm
+    paru -Syu --noconfirm
 
 elif command -v yay &> /dev/null; then
     notify-send "System Update" "Updating AUR packages."
-    sudo yay -Syu --noconfirm
-
+    yay -Syu --noconfirm
+fi
 
 # Notify the user that the update is complete
-notify-send "System Update" "System and AUR packages have been updated."
+notify-send "System Update" "System is updated."
