38adf84274e41024f73136486340713d2d288dd5
ikmrn <ibragimovkmrn@gmail.com>
2024-08-12 11:01:33 +0500

    feat: Add AUR helper check

--- configs/waybar/scripts/update.sh
+++ configs/waybar/scripts/update.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
+
+# Notify the user that the system update is starting
 notify-send "System Update" "Updating your system."
-# Update the system using pacman
-echo "Updating system with pacman..."
-sudo pacman -Syu --noconfirm
 
-# Update AUR packages using paru
-notify-send "System Update" "Updating AUR packages."
+# Update the system using pacman and AUR packages
+if command -v pacman &> /dev/null; then
+    echo "Updating system with pacman..."
+    sudo pacman -Syu --noconfirm
 
-echo "Updating AUR packages with paru..."
-paru -Syu --noconfirm
+elif command -v paru &> /dev/null; then
+    notify-send "System Update" "Updating AUR packages."
+    sudo paru -Syu --noconfirm
 
-# Notify user that the update is complete
+elif command -v yay &> /dev/null; then
+    notify-send "System Update" "Updating AUR packages."
+    sudo yay -Syu --noconfirm
+
+
+# Notify the user that the update is complete
 notify-send "System Update" "System and AUR packages have been updated."
-
-
