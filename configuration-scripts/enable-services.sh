b1363209c9c15e88cf9f358bd049b2a770d1e55d
ikmrn <ibragimovkmrn@gmail.com>
2024-08-13 19:22:23 +0500

    chore: Improve readability

--- configuration-scripts/enable-services.sh
+++ configuration-scripts/enable-services.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+main_dir="$(dirname "$script_dir")"
+source "$main_dir/install-scripts/functions.sh"
+
 systemctl --user enable pipewire.service
 sudo systemctl enable bluetooth.service
 sudo systemctl enable sddm.service
