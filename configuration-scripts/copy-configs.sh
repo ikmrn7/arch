6e6ef80202ac277302d25c72a385b1e799f9cf59
ikmrn <ibragimovkmrn@gmail.com>
2024-08-13 17:52:32 +0500

    feat:  Add script to copy configs

---
+++ configuration-scripts/copy-configs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+main_dir="$(dirname "$script_dir")"
+
+# Copy to .config
+cp -r "$main_dir/configs/.config"* "$HOME/.config" 
+
+# Copy to home directory files only
+find "$main_dir/configs/" -maxdepth 1 -type f -exec cp {} "$HOME" \;
