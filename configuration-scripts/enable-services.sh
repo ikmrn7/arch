f15fc38d5e65ad14a0c8e374212d789823f460e5
ikmrn <ibragimovkmrn@gmail.com>
2024-08-13 18:39:29 +0500

    chore: Add success print message

--- configuration-scripts/enable-services.sh
+++ configuration-scripts/enable-services.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+
 systemctl --user enable pipewire.service
 sudo systemctl enable bluetooth.service
 sudo systemctl enable sddm.service
+
+echo
+print_green "########################################"
+print_green "Services are enabled"
