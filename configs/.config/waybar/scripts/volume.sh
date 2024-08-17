6bad960964cd804fc7bd18cbe83a5e1d2008a991
ikmrn <ibragimovkmrn@gmail.com>
2024-08-17 20:01:05 +0500

    feat: Add unmute function and notification

--- configs/.config/waybar/scripts/volume.sh
+++ configs/.config/waybar/scripts/volume.sh
@@ -13,16 +13,24 @@ SINK=@DEFAULT_SINK@
 
 # Get current volume level
 current_volume=$(pactl get-sink-volume $SINK | awk '{print $5}' | sed 's/%//')
+muted=$(pactl get-sink-mute @DEFAULT_SINK@ | awk '/Mute/ {print $2}')
 
+unmute() {
+  muted=$(pactl get-sink-mute @DEFAULT_SINK@ | awk '/Mute/ {print $2}')
+  if [ "$muted" == "yes" ]; then
+    pactl set-sink-mute $SINK 0
+  notify-send "Volume Unmuted" "The audio has been unmuted." --expire-time=2000
+  fi
+}
 # Adjust volume
 if [ "$1" == "up" ]; then
-    pactl set-sink-mute $SINK 0
+  unmute
   # Check if current volume is less than the limit
   if [ "$current_volume" -lt "$LIMIT" ]; then
     pactl set-sink-volume $SINK +${STEP}%
   fi
 elif [ "$1" == "down" ]; then
-  pactl set-sink-mute $SINK 0
+  unmute
   pactl set-sink-volume $SINK -${STEP}%
 fi
 
