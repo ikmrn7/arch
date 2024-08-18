92bd68b1df448a39bac0c86d430f9efdb4cb4334
ikmrn <ibragimovkmrn@gmail.com>
2024-08-18 22:06:38 +0500

    feat: Neovim initial config

---
+++ configs/.config/nvim/init.lua
@@ -0,0 +1,37 @@
+-- init.lua
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.g.mapleader = " "
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", 
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup("plugins")
+
+local builtin = require('telescope.builtin')
+
+
+
+local config = require("nvim-treesitter.configs")
+config.setup({
+  ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+  highlight = { enable = true },
+  indent = { enable = true },
+})
+
+
+vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
+vim.keymap.set('n', '<leader>fg', builtin.find_files, {})
