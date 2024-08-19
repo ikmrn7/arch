855b904cac7af64c1dbd84f2df92697c004b9d11
ikmrn <ibragimovkmrn@gmail.com>
2024-08-19 13:04:57 +0500

    feat: Add telescope config

---
+++ configs/.config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,11 @@
+-- ~/.config/nvim/lua/plugins/telescope.lua
+
+return {
+    'nvim-telescope/telescope.nvim', tag = '0.1.8',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      local builtin = require('telescope.builtin')
+      vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+      vim.keymap.set('n', '<leader>fg', builtin.find_files, {})
+  end
+}
