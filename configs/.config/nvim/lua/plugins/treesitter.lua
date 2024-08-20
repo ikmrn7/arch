59c1a4532d76e1df779342a10c11ea1a838416e7
ikmrn <ibragimovkmrn@gmail.com>
2024-08-20 22:10:23 +0500

    feat: Add auto-install

--- configs/.config/nvim/lua/plugins/treesitter.lua
+++ configs/.config/nvim/lua/plugins/treesitter.lua
@@ -1,14 +1,15 @@
 -- ~/.config/nvim/lua/plugins/treesitter.lua
 
 return {
-  "nvim-treesitter/nvim-treesitter", 
-  build=":TSUpdate",
-  config = function()
-    local config = require("nvim-treesitter.configs")
-    config.setup({
-      ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "html", "python" },
-      highlight = { enable = true },
-      indent = { enable = true },
-    })
-  end
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local config = require("nvim-treesitter.configs")
+		config.setup({
+			ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "html", "python" },
+			auto_install = true,
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
 }
