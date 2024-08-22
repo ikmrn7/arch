fb232599dbbc16b10230410550e009a4313ab50b
ikmrn <ibragimovkmrn@gmail.com>
2024-08-23 00:05:35 +0500

    feat: Add treesitter config

---
+++ configs/.config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,17 @@
+-- ~/.config/nvim/lua/plugins/treesitter.lua
+
+return {
+	"nvim-treesitter/nvim-treesitter",
+	"nvim-treesitter/playground",
+	build = ":TSUpdate",
+	config = function()
+		local config = require("nvim-treesitter.configs")
+		config.setup({
+			ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "html", "python" },
+			auto_install = true,
+			highlight = { enable = true },
+			indent = { enable = true },
+			additional_vim_regex_highlighting = false
+		})
+	end,
+}
