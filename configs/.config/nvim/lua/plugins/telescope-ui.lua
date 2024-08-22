d5e91b632d9725a2440a5662fbe67b6f2c9d6290
ikmrn <ibragimovkmrn@gmail.com>
2024-08-23 00:04:58 +0500

    feat: Add ui select

---
+++ configs/.config/nvim/lua/plugins/telescope-ui.lua
@@ -0,0 +1,16 @@
+-- ~/.config/nvim/lua/plugins/telescope-ui.lua
+
+return {
+    "nvim-telescope/telescope-ui-select.nvim",
+    config = function()
+        require("telescope").setup({
+            extensions = {
+                ["ui-select"] = {
+                    require("telescope.themes").get_dropdown({}),
+                },
+            },
+        })
+        require("telescope").load_extension("ui-select")
+    end,
+}
+
