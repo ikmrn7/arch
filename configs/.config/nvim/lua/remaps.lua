956412d7b578f178f7ec7caffb369d53213280cb
ikmrn <ibragimovkmrn@gmail.com>
2024-08-22 23:58:16 +0500

    feat: Add keymaps

---
+++ configs/.config/nvim/lua/remaps.lua
@@ -0,0 +1,98 @@
+--------------------------------------------
+--- ALL GENERAL KEYMAPS ARE DEFINED HERE ---
+--- EXCEPTIONS:                          ---
+--- 1. HARPOON                           ---
+--- 2. TELESCOPE                         ---
+--- 3. LSP (PARTIALLY DEFINED HERE)      ---
+--- Specific keymaps for these are       ---
+--- found in their respective            ---
+--- configuration files.                 ---
+--------------------------------------------
+
+
+-- Leader Key Configuration
+vim.g.mapleader = " "
+
+-- General Keymaps
+vim.keymap.set("n", "<leader><leader>", function() vim.cmd("so") end)
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>res", "<cmd>LspRestart<CR>")  -- Restart LSP
+vim.keymap.set("i", "<C-c>", "<Esc>")
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true }) 
+
+
+-- Moving Lines in Visual Mode
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")  -- Move selected line down
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")  -- Move selected line up
+
+
+-- Cursor Positioning Enhancements
+vim.keymap.set("n", "J", "mzJ`z")  -- Join lines while maintaining cursor position
+vim.keymap.set("n", "<C-d>", "<C-d>zz")  -- Scroll down and center screen
+vim.keymap.set("n", "<C-u>", "<C-u>zz")  -- Scroll up and center screen
+vim.keymap.set("n", "n", "nzzzv")  -- Search next and center screen
+vim.keymap.set("n", "N", "Nzzzv")  -- Search previous and center screen
+
+
+-- Clipboard and Deletion Keymaps
+vim.keymap.set("x", "<leader>p", [["_dP]])  -- Paste over selection without overwriting register
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])  -- Yank to system clipboard
+vim.keymap.set("n", "<leader>Y", [["+Y"]])  -- Yank entire line to system clipboard
+vim.keymap.set({"n", "v"}, "<leader>d", [["_d"]])  -- Delete without overwriting register
+
+
+-- Quickfix Navigation
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")  -- Next quickfix and center screen
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")  -- Previous quickfix and center screen
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")  -- Next location list and center screen
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")  -- Previous location list and center screen
+
+
+-- Tmux Integration Keymaps
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+vim.keymap.set("n", "<C-h>", "<Cmd>NvimTmuxNavigateLeft<CR>")
+vim.keymap.set("n", "<C-j>", "<Cmd>NvimTmuxNavigateDown<CR>")
+vim.keymap.set("n", "<C-k>", "<Cmd>NvimTmuxNavigateUp<CR>")
+vim.keymap.set("n", "<C-l>", "<Cmd>NvimTmuxNavigateRight<CR>")
+
+
+-- Search and Replace
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])  -- Replace word under cursor
+
+
+-- Undo Tree Toggle
+vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
+
+
+-- Git Integration Keymaps
+vim.keymap.set("n", "<leader>gs", vim.cmd.Git)  -- Open Git status
+vim.keymap.set("n", "gu", "<cmd>diffget //2<CR>")  -- Get diff from left side
+vim.keymap.set("n", "gh", "<cmd>diffget //3<CR>")  -- Get diff from right side
+
+
+-- LSP Hover Toggle (Floating Window)
+local function toggle_hover()
+  -- Check if there is an existing floating window
+  for _, win in pairs(vim.api.nvim_list_wins()) do
+    local config = vim.api.nvim_win_get_config(win)
+    if config.relative ~= "" then
+      vim.api.nvim_win_close(win, true)
+      return
+    end
+  end
+  -- If no floating window exists, show hover
+  vim.lsp.buf.hover()
+end
+
+
+-- LSP Keymaps
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+vim.keymap.set("n", "K", toggle_hover, { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
+
+
+-- Snippets
+vim.keymap.set("n", "<leader>ee", "oif err != nil {<CR>}<Esc>Oreturn err<Esc>")  -- Insert Go error handling snippet
