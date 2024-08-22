615137fef78c87880ad46eaa4b48a254fcbd9fc7
ikmrn <ibragimovkmrn@gmail.com>
2024-08-22 23:59:39 +0500

    feat: Add alpha config

---
+++ configs/.config/nvim/lua/plugins/alpha-nvim.lua
@@ -0,0 +1,9 @@
+-- ~/.config/nvim/lua/plugins/alpha-nvim.lua
+
+return {
+  'goolord/alpha-nvim',
+  dependencies = { 'echasnovski/mini.icons' },
+  config = function ()
+      require'alpha'.setup(require'alpha.themes.startify'.config)
+  end
+  };
