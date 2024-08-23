a811487ae5ffe67d4b62124c8f28b21910253b39
ikmrn <ibragimovkmrn@gmail.com>
2024-08-23 10:07:03 +0500

    chore: Improve indenting for readability

--- configs/.config/zsh/aliases.zsh
+++ configs/.config/zsh/aliases.zsh
@@ -2,13 +2,13 @@
 ### Personal aliases ###
 ########################
 
-alias c="code"                                 # Open VS Code
-alias t="touch"                                # Create a new file
-alias cx="clear"                               # Clear terminal
-alias n="nvim"                                 # Open Neovim
-alias cs="~/.config/auto-commit.sh"            # Auto commit script
-alias chwp="~/.config/change-wallpaper.sh"     # Script to change wallpaper
-alias addw="~/.config/aspell-add-word.sh"      # Script to add words into personal aspell dict
+alias c="code"                                                                # Open VS Code
+alias t="touch"                                                               # Create a new file
+alias cx="clear"                                                              # Clear terminal
+alias n="nvim"                                                                # Open Neovim
+alias cs="~/.config/auto-commit.sh"                                           # Auto commit script
+alias chwp="~/.config/change-wallpaper.sh"                                    # Script to change wallpaper
+alias addw="~/.config/aspell-add-word.sh"                                     # Script to add words into personal aspell dict
 alias dolphinenable="systemctl --user start kbuildsycoca.service"             # Enable dolphin default apps
 
 
