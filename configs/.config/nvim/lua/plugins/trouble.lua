f247e0b54a6868369d44f7731e06b7b1a5389933
ikmrn <ibragimovkmrn@gmail.com>
2024-08-27 12:46:33 +0500

    feat: Add trouble config

---
+++ configs/.config/nvim/lua/plugins/trouble.lua
@@ -0,0 +1,7 @@
+-- ~/.config/nvim/lua/plugins/trouble.lua
+
+return {
+  "folke/trouble.nvim",
+  opts = {}, -- for default options, refer to the configuration section for custom setup.
+  cmd = "Trouble",
+}
