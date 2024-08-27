9dddb380fe47954a1bc63733757b64ec74245875
ikmrn <ibragimovkmrn@gmail.com>
2024-08-27 20:29:07 +0500

    feat: Change startify to dashboard

--- configs/.config/nvim/lua/plugins/alpha-nvim.lua
+++ configs/.config/nvim/lua/plugins/alpha-nvim.lua
@@ -4,6 +4,30 @@ return {
   'goolord/alpha-nvim',
   dependencies = { 'echasnovski/mini.icons' },
   config = function ()
-      require'alpha'.setup(require'alpha.themes.startify'.config)
+    local alpha = require("alpha")
+local dashboard = require("alpha.themes.dashboard")
+
+-- Set header
+dashboard.section.header.val = {
+    "                                                     ",
+    "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
+    "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
+    "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
+    "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
+    "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
+    "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
+    "                                                     ",
+}
+
+-- Set menu
+dashboard.section.buttons.val = {
+    dashboard.button( "e", "  > New file" , ":ene <BAR> startinsert <CR>"),
+    dashboard.button( "f", "  > Find file", ":cd $HOME | Telescope find_files<CR>"),
+    dashboard.button( "r", "  > Recent"   , ":Telescope oldfiles<CR>"),
+    dashboard.button( "s", "  > Settings" , ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
+}
+
+alpha.setup(dashboard.opts)
+
   end
   };
