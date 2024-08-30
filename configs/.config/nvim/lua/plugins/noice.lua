f4f698df1c77307637e83ab14bfd0a7706e19d3f
ikmrn <ibragimovkmrn@gmail.com>
2024-08-30 17:35:40 +0500

    fix: Add notify for recording a macro

--- configs/.config/nvim/lua/plugins/noice.lua
+++ configs/.config/nvim/lua/plugins/noice.lua
@@ -1,8 +1,19 @@
 return {
-    "folke/noice.nvim",
-    event = "VeryLazy",
-    opts = {},
-    dependencies = {
-        "MunifTanjim/nui.nvim",
-    },
+	"folke/noice.nvim",
+	event = "VeryLazy",
+	opts = {
+	},
+	dependencies = {
+		"MunifTanjim/nui.nvim",
+		-- OPTIONAL:
+		-- "rcarriga/nvim-notify",
+	},
+	require("noice").setup({
+		routes = {
+			{
+				view = "notify",
+				filter = { event = "msg_showmode" },
+			},
+		},
+	}),
 }
