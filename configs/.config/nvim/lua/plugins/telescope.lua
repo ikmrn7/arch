451e41773383bd7f5a38d75cc3cb15d8f453ae33
ikmrn <ibragimovkmrn@gmail.com>
2024-08-30 17:36:30 +0500

    feat: Change keymaps

--- configs/.config/nvim/lua/plugins/telescope.lua
+++ configs/.config/nvim/lua/plugins/telescope.lua
@@ -11,19 +11,19 @@ return {
         ---------------
         --- KEYMAPS ---
         ---------------
-        vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
-        vim.keymap.set("n", "<C-p>", builtin.git_files, {})
-        vim.keymap.set("n", "<leader>pws", function()
+        vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+        vim.keymap.set("n", "<leader>fg", builtin.git_files, {})
+        vim.keymap.set("n", "<leader>fws", function()
             local word = vim.fn.expand("<cword>")
             builtin.grep_string({ search = word })
         end)
-        vim.keymap.set("n", "<leader>pWs", function()
+        vim.keymap.set("n", "<leader>fWs", function()
             local word = vim.fn.expand("<cWORD>")
             builtin.grep_string({ search = word })
         end)
-        vim.keymap.set("n", "<leader>ps", function()
+        vim.keymap.set("n", "<leader>fgr", function()
             builtin.grep_string({ search = vim.fn.input("Grep > ") })
         end)
-        vim.keymap.set("n", "<leader>vh", builtin.help_tags, {})
+        vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
     end,
 }
