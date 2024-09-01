1503a6b60461eb0959dd893a05a6f51ec5446d0d
ikmrn <ibragimovkmrn@gmail.com>
2024-09-01 19:17:54 +0500

    feat: Add npm packages install script

---
+++ install-scripts/npm-packages-install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+##############################
+### NPM Packages Installer ###
+##############################
+
+# This script installs and npm packages.
+source "$(dirname "$0")/functions.sh"
+
+NPM_PACKAGES=(
+"eslint_d"              # JS static analysis tool
+)
+
+read -p "Do you want to install npm packages? (Y/n) " install_choice
+install_choice=${install_choice:-Y}
+
+
+if [[ $install_choice =~ ^[Yy] ]] && pkg_installed "npm"; then
+    for pkg in "${NPM_PACKAGES[@]}"; do
+      npm install -g "$pkg"
+    done
+else
+    echo "Installation skipped or npm is not installed."
+fi
+
+echo
+print_green "########################################"
+print_green "NPM packages installation complete!"
+
