c538f9ea7525ee2ebf4ea80993fce1ceaced2c9e
ikmrn <ibragimovkmrn@gmail.com>
2024-09-02 23:19:31 +0500

    feat: Remove go lsp

--- configs/.config/nvim/lua/plugins/lsp.lua
+++ configs/.config/nvim/lua/plugins/lsp.lua
@@ -29,7 +29,7 @@ return {
             ensure_installed = {
                 "lua_ls",
                 "rust_analyzer",
-                "gopls",
+                -- "gopls",
                 "tsserver",
                 "cssls",
                 "html",
