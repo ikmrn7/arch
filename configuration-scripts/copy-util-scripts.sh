bfca4a9ae2f967b7f1a9b747715884f69cc1f6ab
ikmrn <ibragimovkmrn@gmail.com>
2024-09-05 01:19:07 +0500

    refactor: Add script path in stow.sh

--- configuration-scripts/copy-util-scripts.sh
+++ configuration-scripts/copy-util-scripts.sh
@@ -7,22 +7,17 @@
 # This script sets up utility scripts by making them executable and copying 
 # them to the appropriate configuration directory. It ensures that all utility 
 # scripts are ready to use in your environment.
-# Additionally, add the stow script path to the Zsh configuration file
 
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 main_dir="$(dirname "$script_dir")"
 utility_scripts_dir="$main_dir/utility-scripts"
 source "$main_dir/install-scripts/functions.sh"
 
-# for file in "$utility_scripts_dir"/*
-# do
-#   make_executable "$file"
-#   cp "$file" "$HOME/.config"
-# done
-
-# Add stow script path
-sed -i "s|stowconf=\"stow.sh\"|stowconf=\"$script_dir/stow.sh\"|" "$main_dir/configs/.config/zsh/aliases.zsh"
-
+for file in "$utility_scripts_dir"/*
+do
+  make_executable "$file"
+  cp "$file" "$HOME/.config"
+done
 
 echo
 print_green "########################################"
