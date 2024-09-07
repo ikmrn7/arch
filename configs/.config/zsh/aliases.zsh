4d1ac491530fc953094f4179ee92e01810e61160
ikmrn <ibragimovkmrn@gmail.com>
2024-09-07 21:28:29 +0500

    refactor: Remove stow path
    Use stowconf script in ~/.local/bin to create symlinks.

--- configs/.config/zsh/aliases.zsh
+++ configs/.config/zsh/aliases.zsh
@@ -14,8 +14,6 @@ alias dolphinenable="systemctl --user start kbuildsycoca.service"             # 
 
 
 # Zsh configuration management
-alias zshconfig="code ~/.zshrc"                                        # Edit Zsh configuration
 alias in='nvim $(fzf -m --preview="bat --color=always {}")'            # Open file with fzf preview
 alias sudoin='sudo nvim $(fzf -m --preview="bat --color=always {}")'   # Edit file with sudo and fzf preview
 alias re="source ~/.zshrc"                                             # Reload Zsh configuration
-alias stowconf="stow.sh" # Create symlinks with stow
