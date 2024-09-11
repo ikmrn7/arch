8d157ccdcc2debf60b994c45aae87a27cfe5e540
ikmrn <ibragimovkmrn@gmail.com>
2024-09-11 16:00:08 +0500

    chore: Fix indentation for readability

--- configuration-scripts/stow.sh
+++ configuration-scripts/stow.sh
@@ -18,7 +18,7 @@ bin_dir="$HOME/.local/bin"
 source "$main_dir/install-scripts/functions.sh"
 
 # Call the prompt_timer function with a timeout of 10 seconds and custom message
-response_timer 10 "Do you want to create symlinks of your configs with stow? (Y/n)"
+response_timer 10 "Do you want to create symlinks of your configs with stow? (Y/n)" response
 
 # Use the user input or default to 'Y' if no input is provided
 response=${response:-Y}
@@ -39,17 +39,17 @@ if [[ "$response" =~ ^[Yy]$ ]]; then
   mkdir -p "$bin_dir"
 
   # Add stow scripts to bin
-echo "#!/bin/bash
-stow -d \"$configs_dir\" --adopt -t \"$HOME/.config\" .config
-stow -d \"$configs_dir\" --adopt -t \"$HOME\" home
-stow -d \"$main_dir\" --adopt -t \"$HOME/.config\" utility-scripts" > "$bin_dir/stowconf"
+  echo "#!/bin/bash
+  stow -d \"$configs_dir\" --adopt -t \"$HOME/.config\" .config
+  stow -d \"$configs_dir\" --adopt -t \"$HOME\" home
+  stow -d \"$main_dir\" --adopt -t \"$HOME/.config\" utility-scripts" > "$bin_dir/stowconf"
 
-# Make the script executable
-chmod +x "$bin_dir/stowconf"
+  # Make the script executable
+  chmod +x "$bin_dir/stowconf"
 
-echo
-print_green "########################################"
-print_green "Symlinks created"
+  echo
+  print_green "########################################"
+  print_green "Symlinks created"
 else
   echo
   print_green "########################################"
