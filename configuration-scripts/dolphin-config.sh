b6bea714ec87c8bbf91fbaea6081f9eeb3897f13
ikmrn <ibragimovkmrn@gmail.com>
2024-09-11 14:11:30 +0500

    feat: Script to add dolphin icon config

---
+++ configuration-scripts/dolphin-config.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+###################################
+### Dolphin Configuration Setup ###
+###################################
+
+# This script ensures that the '[Icons]' section in the Dolphin configuration file
+# contains the 'Theme=Papirus-Dark' line.
+FILE="$HOME/.config/dolphinrc"
+
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+install_dir="$(dirname "$script_dir")/install-scripts"
+
+source "$install_dir/functions.sh"
+
+if [ ! -f "$FILE" ]; then
+  print_red "The file $FILE does not exist"
+  exit 1
+fi
+
+if grep -q "^\[Icons\]" "$FILE"; then
+  if grep -q "^Theme=Papirus-Dark" "$FILE"; then
+    print_green "Theme is already set to Papirus-Dark"
+  else
+    if  sed -i "/^\[Icons\]/a Theme=Papirus-Dark" "$FILE"; then
+      print_green "Papirus-Dark icon theme is added"
+    else
+      print_red "Failed to add icon theme"
+      exit 1
+    fi
+  fi
+else 
+  if echo -e "\n[Icons]\nTheme=Papirus-Dark" >> "$FILE"; then
+    print_green "Added the [Icons] section with 'Theme=Papirus-Dark'."
+  else
+    print_red "Failed to add the [Icons] section with 'Theme=Papirus-Dark'"
+  fi
+fi
