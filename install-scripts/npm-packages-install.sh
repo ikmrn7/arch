5268141dbca51d63a01472372d6e34d108b3b5a6
ikmrn <ibragimovkmrn@gmail.com>
2024-09-11 16:02:00 +0500

    feat: Add check for fail

--- install-scripts/npm-packages-install.sh
+++ install-scripts/npm-packages-install.sh
@@ -4,27 +4,40 @@
 ### NPM Packages Installer ###
 ##############################
 
-# This script installs and npm packages.
+# This script installs npm packages.
 source "$(dirname "$0")/functions.sh"
 
 NPM_PACKAGES=(
 "eslint_d"              # JS static analysis tool
 "async"                 # Module to work with async JS
+"lodash"
 )
 
-read -p "Do you want to install npm packages? (Y/n) " install_choice
-install_choice=${install_choice:-Y}
-
-
-if [[ $install_choice =~ ^[Yy] ]] && pkg_installed "npm"; then
+response_timer 10 "Do you want to install npm packages? (Y/n) " response
+response=${response:-Y}
+success=true
+# Function to install npm packages
+install_npm_packages() {
     for pkg in "${NPM_PACKAGES[@]}"; do
-      sudo npm install -g "$pkg"
+        if ! sudo npm install -g "$pkg"; then
+            print_red "Failed to install $pkg."
+            success=false
+        else
+            print_green "Successfully installed $pkg."
+        fi
     done
+
+}
+
+# Check if npm is installed and if user wants to proceed
+if [[ $response =~ ^[Yy] ]] && pkg_installed "npm"; then
+    if $success; then
+        print_green "########################################"
+        print_green "NPM packages installation complete!"
+    else
+        print_red "########################################"
+        print_red "NPM packages installation encountered errors."
+    fi
 else
-    echo "Installation skipped or npm is not installed."
+    print_red "Installation skipped or npm is not installed."
 fi
-
-echo
-print_green "########################################"
-print_green "NPM packages installation complete!"
-
