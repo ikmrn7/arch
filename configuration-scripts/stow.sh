b137485a1eec7940f41ac2ffabe406e374188a03
ikmrn <ibragimovkmrn@gmail.com>
2024-09-11 12:00:58 +0500

    feat: Add timer

--- configuration-scripts/stow.sh
+++ configuration-scripts/stow.sh
@@ -6,21 +6,23 @@
 
 # This script creates symbolic links for configuration files using stow.
 # It prompts you to confirm whether to create symlinks for configurations
-# in the `.config` directory and home directory. Ensure that stow and
+# in the `.config` directory and home directory. Ensure stow and
 # the relevant configuration directories are properly set up before running.
 
-# Define directories
+
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 main_dir="$(dirname "$script_dir")"
 configs_dir="$main_dir/configs"
 bin_dir="$HOME/.local/bin"
 
-# Source functions
 source "$main_dir/install-scripts/functions.sh"
 
-# Prompt user for confirmation
-read -r -p "Do you want to create symlinks of your configs with stow? (Y/n): " response
+# Call the prompt_timer function with a timeout of 10 seconds and custom message
+response_timer 10 "Do you want to create symlinks of your configs with stow? (Y/n)"
+
+# Use the user input or default to 'Y' if no input is provided
 response=${response:-Y}
+
 
 if [[ "$response" =~ ^[Yy]$ ]]; then
   # Create .config directory symlinks with stow
@@ -30,27 +32,26 @@ if [[ "$response" =~ ^[Yy]$ ]]; then
   # Create home directory symlinks with stow
   stow -d "$configs_dir" --adopt -t "$HOME" home
 
-  # Create utility scripts symlinks with stow
+  # Create home directory symlinks with stow
   stow -d "$main_dir" --adopt -t "$HOME/.config" utility-scripts
 
   # Ensure bin directory exists
   mkdir -p "$bin_dir"
 
   # Add stow scripts to bin
-  echo "#!/bin/bash
+echo "#!/bin/bash
 stow -d \"$configs_dir\" --adopt -t \"$HOME/.config\" .config
 stow -d \"$configs_dir\" --adopt -t \"$HOME\" home
 stow -d \"$main_dir\" --adopt -t \"$HOME/.config\" utility-scripts" > "$bin_dir/stowconf"
 
-  # Make the script executable
-  chmod +x "$bin_dir/stowconf"
+# Make the script executable
+chmod +x "$bin_dir/stowconf"
 
-  echo
-  print_green "########################################"
-  print_green "Symlinks created"
+echo
+print_green "########################################"
+print_green "Symlinks created"
 else
   echo
   print_green "########################################"
   print_green "Skipping symlink creation."
 fi
-
