e0fe2d8be155ddd05acfd0a6a8cdebfac5f72134
i-kamran <ibragimovkmrn@gmail.com>
2024-09-19 23:13:37 +0500

    fix: Disable overlapping unused mappings

--- configs/.config/nvim/lua/plugins/visual-multi.lua
+++ configs/.config/nvim/lua/plugins/visual-multi.lua
@@ -1,3 +1,11 @@
 return {
-    'mg979/vim-visual-multi'
+    'mg979/vim-visual-multi',
+    config = function()
+        -- Disable specific key mappings for this plugin
+        vim.g['VM_maps'] = {
+            ['Goto Next'] = '',
+            ['Goto Prev'] = '',
+            ['Toggle Block'] = '',
+        }
+    end
 }
