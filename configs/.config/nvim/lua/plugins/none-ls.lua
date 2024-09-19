e85e74eb0291931db48857f7ae5fdcf97133b0ed
i-kamran <ibragimovkmrn@gmail.com>
2024-09-19 10:49:48 +0500

    style: Format for better readability

--- configs/.config/nvim/lua/plugins/none-ls.lua
+++ configs/.config/nvim/lua/plugins/none-ls.lua
@@ -1,44 +1,51 @@
 return {
-	"nvimtools/none-ls.nvim",
-	dependencies = {
-		"williamboman/mason.nvim",
-		"jay-babu/mason-null-ls.nvim",
-		"nvimtools/none-ls-extras.nvim",
-	},
-	config = function()
-		local null_ls = require("null-ls")
-		local mason_null_ls = require("mason-null-ls")
+    "nvimtools/none-ls.nvim",
+    dependencies = {
+        "williamboman/mason.nvim",
+        "jay-babu/mason-null-ls.nvim",
+        "nvimtools/none-ls-extras.nvim",
+    },
+    config = function()
+        local null_ls = require("null-ls")
+        local mason_null_ls = require("mason-null-ls")
 
-		mason_null_ls.setup({
-			ensure_installed = {
-				-- Formatters
-				"prettierd",
-				"stylua",
-				"isort",
-				"black",
+        mason_null_ls.setup({
+            ensure_installed = {
+                -- Formatters
+                "prettierd",
+                "stylua",
+                "isort",
+                "black",
 
-				-- Linters
-				"eslint_d",
-				"flake8",
-				"shellcheck",
-			},
-			automatic_setup = true,
-		})
+                -- Linters
+                "eslint_d",
+                "flake8",
+                "shellcheck"
+            },
+            automatic_setup = true,
+        })
 
-		null_ls.setup({
-			sources = {
-				-- Formatters
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.prettierd,
-				null_ls.builtins.formatting.isort,
-				null_ls.builtins.formatting.black,
-				-- Linters
-				require("none-ls.diagnostics.eslint_d"),
-				require("none-ls.diagnostics.flake8"),
+        null_ls.setup({
+            sources = {
+                -- Formatters
+                null_ls.builtins.formatting.stylua,
+                null_ls.builtins.formatting.prettierd,
+                null_ls.builtins.formatting.isort,
+                null_ls.builtins.formatting.black,
 
-				null_ls.builtins.completion.spell,
-				-- null_ls.builtins.diagnostics.shellcheck,
-			},
-		})
-	end,
+                -- Linters
+                require("none-ls.diagnostics.eslint_d"),
+                require("none-ls.diagnostics.flake8"),
+
+                null_ls.builtins.completion.spell,
+            },
+            -- Exclude C and C++ files
+            on_attach = function(client, bufnr)
+                local filetype = vim.api.nvim_buf_get_option(bufnr, "filetype")
+                if filetype == "c" or filetype == "cpp" then
+                    client.stop()
+                end
+            end,
+        })
+    end,
 }
