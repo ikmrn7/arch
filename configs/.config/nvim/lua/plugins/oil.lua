f8de6c3658aa8ca858cb8b7f6eefb8eba766e6c1
i-kamran <ibragimovkmrn@gmail.com>
2024-10-09 12:11:28 +0500

    feat: Add oil.nvim plugin

---
+++ configs/.config/nvim/lua/plugins/oil.lua
@@ -0,0 +1,21 @@
+return {
+    "stevearc/oil.nvim",
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {},
+    -- Optional dependencies
+    dependencies = { { "echasnovski/mini.icons", opts = {} } },
+    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+    config = function()
+        require("oil").setup({
+            columns = {
+                "icon",
+                "size",
+            },
+            view_options = {
+                -- Show files and directories that start with "."
+                show_hidden = true,
+            },
+        })
+    end,
+}
