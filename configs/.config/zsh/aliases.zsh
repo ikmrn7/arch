51686c5ce773eb77ae97703d9a5c08e275b42a8f
i-kamran <ibragimovkmrn@gmail.com>
2024-10-09 15:09:45 +0500

    feat: Add zsh alias for dolphinenable

--- configs/.config/zsh/aliases.zsh
+++ configs/.config/zsh/aliases.zsh
@@ -6,6 +6,7 @@ alias vim="nvim"                                                              # 
 alias c="code"                                                                # Open VS Code
 alias t="touch"                                                               # Create a new file
 alias cx="clear"                                                              # Clear terminal
+alias de="dolphinenable"
 alias cs="~/.config/auto-commit.sh"                                           # Auto commit script
 alias chwp="~/.config/change-wallpaper.sh"                                    # Script to change wallpaper
 alias addw="~/.config/aspell-add-word.sh"                                     # Script to add words into personal aspell dict
