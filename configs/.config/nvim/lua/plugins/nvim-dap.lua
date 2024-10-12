6ea91e3bbdb34bb17693143b8028b4412822520b
i-kamran <ibragimovkmrn@gmail.com>
2024-10-12 22:14:41 +0500

    feat: Move keymaps to remaps.lua

--- configs/.config/nvim/lua/plugins/nvim-dap.lua
+++ configs/.config/nvim/lua/plugins/nvim-dap.lua
@@ -1,32 +1,30 @@
 return {
-	"mfussenegger/nvim-dap",
-	lazy = true,
-	event = "VeryLazy",
-	dependencies = {
-		"rcarriga/nvim-dap-ui",
-		"nvim-neotest/nvim-nio",
-		"leoluz/nvim-dap-go",
-		"mfussenegger/nvim-dap-python",
-	},
-	config = function()
-		local dap, dapui = require("dap"), require("dapui")
-		vim.keymap.set("n", "<leader>bt", dap.toggle_breakpoint, {})
-		vim.keymap.set("n", "<leader>bc", dap.continue, {})
+    "mfussenegger/nvim-dap",
+    lazy = true,
+    event = "VeryLazy",
+    dependencies = {
+        "rcarriga/nvim-dap-ui",
+        "nvim-neotest/nvim-nio",
+        "leoluz/nvim-dap-go",
+        "mfussenegger/nvim-dap-python",
+    },
+    config = function()
+        local dap, dapui = require("dap"), require("dapui")
 
-		require("dap-python").setup("~/.venv/bin/python")
-		require("dap-go").setup()
-		require("dapui").setup()
-		dap.listeners.before.attach.dapui_config = function()
-			dapui.open()
-		end
-		dap.listeners.before.launch.dapui_config = function()
-			dapui.open()
-		end
-		dap.listeners.before.event_terminated.dapui_config = function()
-			dapui.close()
-		end
-		dap.listeners.before.event_exited.dapui_config = function()
-			dapui.close()
-		end
-	end,
+        require("dap-python").setup("~/.venv/bin/python")
+        require("dap-go").setup()
+        require("dapui").setup()
+        dap.listeners.before.attach.dapui_config = function()
+            dapui.open()
+        end
+        dap.listeners.before.launch.dapui_config = function()
+            dapui.open()
+        end
+        dap.listeners.before.event_terminated.dapui_config = function()
+            dapui.close()
+        end
+        dap.listeners.before.event_exited.dapui_config = function()
+            dapui.close()
+        end
+    end,
 }
