029dfd5d022fc6a3dc55224715d7cdcabb317f34
i-kamran <ibragimovkmrn@gmail.com>
2024-10-12 23:20:52 +0500

    feat: Do not conceal code blocks in Markdown

--- configs/.config/nvim/lua/plugins/treesitter.lua
+++ configs/.config/nvim/lua/plugins/treesitter.lua
@@ -11,6 +11,16 @@ return {
             highlight = { enable = true },
             indent = { enable = true },
             additional_vim_regex_highlighting = false,
+            require("vim.treesitter.query").set(
+                "markdown",
+                "highlights",
+                [[
+                    ;From MDeiml/tree-sitter-markdown
+                    [
+                      (fenced_code_block_delimiter)
+                    ] @punctuation.delimiter
+                ]]
+            ),
         })
     end,
 }
