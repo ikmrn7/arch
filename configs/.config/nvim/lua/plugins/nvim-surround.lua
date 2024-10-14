e57212537ac2ebfbd69c593b263fcfb4869b36fb
i-kamran <ibragimovkmrn@gmail.com>
2024-10-14 23:50:02 +0500

    feat: Organize auto editing in smart-edit.lua

--- configs/.config/nvim/lua/plugins/nvim-surround.lua
+++
@@ -1,8 +0,0 @@
--- ~/.config/nvim/lua/plugins/nvim-surround.lua
-
-return {
-    "kylechui/nvim-surround",
-    config = function()
-        require("nvim-surround").setup({ version = "*", event = "VeryLazy", opts = {} })
-    end,
-}
