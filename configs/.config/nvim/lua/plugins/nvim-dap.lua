13ff0916be9cdb1d6bd49234385bdd2197d5d9b3
i-kamran <ibragimovkmrn@gmail.com>
2024-10-15 20:27:49 +0500

    feat: Mason DAP python, go, c, cpp, rust configs

--- configs/.config/nvim/lua/plugins/nvim-dap.lua
+++ configs/.config/nvim/lua/plugins/nvim-dap.lua
@@ -1,3 +1,5 @@
+-- ~/.config/nvim/lua/plugins/nvim-dap.lua
+
 return {
     "mfussenegger/nvim-dap",
     lazy = true,
@@ -5,15 +7,28 @@ return {
     dependencies = {
         "rcarriga/nvim-dap-ui",
         "nvim-neotest/nvim-nio",
-        "leoluz/nvim-dap-go",
-        "mfussenegger/nvim-dap-python",
+        "williamboman/mason.nvim",
+        "jay-babu/mason-nvim-dap.nvim",
     },
     config = function()
         local dap, dapui = require("dap"), require("dapui")
 
-        require("dap-python").setup("~/.venv/bin/python")
-        require("dap-go").setup()
-        require("dapui").setup()
+        -- Initialize mason and mason-nvim-dap
+        require("mason").setup()
+        require("mason-nvim-dap").setup({
+            ensure_installed = { "codelldb", "debugpy", "delve" },
+            automatic_installation = true,
+            handlers = {
+                function(config)
+                    require("mason-nvim-dap").default_setup(config)
+                end,
+            },
+        })
+
+        -- UI setup
+        dapui.setup()
+
+        -- Listeners to auto-open/close dap-ui
         dap.listeners.before.attach.dapui_config = function()
             dapui.open()
         end
@@ -26,5 +41,45 @@ return {
         dap.listeners.before.event_exited.dapui_config = function()
             dapui.close()
         end
+
+        -- Python setup
+        dap.configurations.python = {
+            {
+                type = "python",
+                request = "launch",
+                name = "Launch file",
+                program = "${file}",
+                pythonPath = function()
+                    return "/usr/bin/python"
+                end,
+            },
+        }
+
+        -- Go setup
+        dap.configurations.go = {
+            {
+                type = "delve",
+                name = "Debug",
+                request = "launch",
+                program = "${file}"
+            },
+        }
+
+        -- C, C++, and Rust setup using codelldb
+        dap.configurations.c = {
+            {
+                name = "Launch file",
+                type = "codelldb",
+                request = "launch",
+                program = function()
+                    return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+                end,
+                cwd = '${workspaceFolder}',
+                stopOnEntry = false,
+            },
+        }
+
+        dap.configurations.cpp = dap.configurations.c
+        dap.configurations.rust = dap.configurations.c
     end,
 }
