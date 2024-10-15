42c235c7fcbac30fe637138f0cbea8945873a4de
i-kamran <ibragimovkmrn@gmail.com>
2024-10-15 12:56:34 +0500

    refactor: split interface configs, move some to visual.lua

--- configs/.config/nvim/lua/plugins/interface.lua
+++ configs/.config/nvim/lua/plugins/interface.lua
@@ -1,4 +1,4 @@
--- ~/.config/nvim/lua/plugins/visual.lua
+-- ~/.config/nvim/lua/plugins/interface.lua
 
 return {
     -- undo tree
@@ -13,53 +13,6 @@ return {
         event = "VeryLazy",
         opts = {}, -- for default options, refer to the configuration section for custom setup.
         cmd = "Trouble",
-    },
-
-    -- vim-illuminate
-    {
-        "RRethy/vim-illuminate",
-        lazy = true,
-        event = "VeryLazy",
-    },
-
-    -- indent-blankline
-    {
-        -- Add indentation guides even on blank lines
-        "lukas-reineke/indent-blankline.nvim",
-        -- Enable `lukas-reineke/indent-blankline.nvim`
-        -- See `:help ibl`
-        main = "ibl",
-        opts = {},
-        config = function()
-            require("ibl").setup()
-        end,
-    },
-
-    -- visual-multi
-    {
-        "mg979/vim-visual-multi",
-        lazy = true,
-        event = "VeryLazy",
-        config = function()
-            -- Disable specific key mappings for this plugin
-            vim.g["VM_maps"] = {
-                ["Goto Next"] = "",
-                ["Goto Prev"] = "",
-                ["Toggle Block"] = "",
-            }
-        end,
-    },
-
-    -- todo-comments
-    {
-        "folke/todo-comments.nvim",
-        lazy = true,
-        event = "VeryLazy",
-        dependencies = { "nvim-lua/plenary.nvim" },
-        opts = {
-            -- your configuration comes here
-            -- or leave it empty to use the default settings
-        },
     },
 
     -- which-key
@@ -80,18 +33,6 @@ return {
                 desc = "Buffer Local Keymaps (which-key)",
             },
         },
-    },
-
-    -- highlight-colors
-    {
-        "brenoprata10/nvim-highlight-colors",
-        config = function()
-            require("cmp").setup({
-                formatting = {
-                    format = require("nvim-highlight-colors").format,
-                },
-            })
-        end,
     },
 
     -- noice
@@ -177,60 +118,6 @@ return {
                     theme = custom_theme,
                 },
             })
-        end,
-    },
-
-    -- catppuccin/nvim
-    {
-        "catppuccin/nvim",
-        lazy = false,
-        name = "catppuccin",
-        priority = 1000,
-        config = function()
-            -- Define the custom palette
-            -- local mocha = require("catppuccin.palettes").get_palette("mocha")
-
-            -- Setup Catppuccin with custom colors and integrations
-            require("catppuccin").setup({
-                color_overrides = {
-                    mocha = {
-                        text = "#E8E4D6",
-                        base = "#1C1C19",
-                        mantle = "#171512",
-                        crust = "#0D0D0B",
-                        surface0 = "#332F2E",
-                        surface1 = "#4D4645",
-                        surface2 = "#665D5C",
-                        overlay0 = "#8C8685",
-                        overlay1 = "#B3ABAA",
-                        overlay2 = "#D9CFCE",
-                        subtext0 = "#D9D5C7",
-                        subtext1 = "#BFBCB0",
-                        blue = "#8DACE2",
-                        green = "#A1CC85",
-                        yellow = "#F2DEA6",
-                        red = "#E67373",
-                        lavander = "#DCB8E6",
-                        teal = "#ABEDE4",
-                        peach = "#EAAE6F",
-                        sapphire = "#7ACCC1",
-                    },
-                },
-                integrations = {
-                    cmp = true,
-                    gitsigns = true,
-                    nvimtree = true,
-                    treesitter = true,
-                    notify = false,
-                    mini = {
-                        enabled = true,
-                        indentscope_color = "",
-                    },
-                },
-            })
-
-            -- Apply the Catppuccin colorscheme
-            vim.cmd.colorscheme("catppuccin")
         end,
     },
 
---
+++ configs/.config/nvim/lua/plugins/visual.lua
@@ -0,0 +1,102 @@
+-- ~/.config/nvim/lua/plugins/visual.lua
+
+return {
+    -- vim-illuminate
+    {
+        "RRethy/vim-illuminate",
+        lazy = true,
+        event = "VeryLazy",
+    },
+
+    -- todo-comments
+    {
+        "folke/todo-comments.nvim",
+        lazy = true,
+        event = "VeryLazy",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        opts = {
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+        },
+    },
+
+
+    -- indent-blankline
+    {
+        -- Add indentation guides even on blank lines
+        "lukas-reineke/indent-blankline.nvim",
+        -- Enable `lukas-reineke/indent-blankline.nvim`
+        -- See `:help ibl`
+        main = "ibl",
+        opts = {},
+        config = function()
+            require("ibl").setup()
+        end,
+    },
+
+    -- highlight-colors
+    {
+        "brenoprata10/nvim-highlight-colors",
+        config = function()
+            require("cmp").setup({
+                formatting = {
+                    format = require("nvim-highlight-colors").format,
+                },
+            })
+        end,
+    },
+
+    -- catppuccin/nvim
+    {
+        "catppuccin/nvim",
+        lazy = false,
+        name = "catppuccin",
+        priority = 1000,
+        config = function()
+            -- Define the custom palette
+            -- local mocha = require("catppuccin.palettes").get_palette("mocha")
+
+            -- Setup Catppuccin with custom colors and integrations
+            require("catppuccin").setup({
+                color_overrides = {
+                    mocha = {
+                        text = "#E8E4D6",
+                        base = "#1C1C19",
+                        mantle = "#171512",
+                        crust = "#0D0D0B",
+                        surface0 = "#332F2E",
+                        surface1 = "#4D4645",
+                        surface2 = "#665D5C",
+                        overlay0 = "#8C8685",
+                        overlay1 = "#B3ABAA",
+                        overlay2 = "#D9CFCE",
+                        subtext0 = "#D9D5C7",
+                        subtext1 = "#BFBCB0",
+                        blue = "#8DACE2",
+                        green = "#A1CC85",
+                        yellow = "#F2DEA6",
+                        red = "#E67373",
+                        lavander = "#DCB8E6",
+                        teal = "#ABEDE4",
+                        peach = "#EAAE6F",
+                        sapphire = "#7ACCC1",
+                    },
+                },
+                integrations = {
+                    cmp = true,
+                    gitsigns = true,
+                    nvimtree = true,
+                    treesitter = true,
+                    notify = false,
+                    mini = {
+                        enabled = true,
+                        indentscope_color = "",
+                    },
+                },
+            })
+
+            -- Apply the Catppuccin colorscheme
+            vim.cmd.colorscheme("catppuccin")
+        end,
+    },
+}
