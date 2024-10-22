9adf1f05f23f0bc34db3834739e984a041a87f11
i-kamran <ibragimovkmrn@gmail.com>
2024-10-22 17:05:04 +0500

    feat: Add zsh alises for reboot and shutdown now

--- configs/.config/zsh/aliases.zsh
+++ configs/.config/zsh/aliases.zsh
@@ -6,6 +6,8 @@ alias vim="nvim"                                                              # 
 alias c="code"                                                                # Open VS Code
 alias t="touch"                                                               # Create a new file
 alias cx="clear"                                                              # Clear terminal
+alias sht="shutdown now"                                                      # Shutdown
+alias re="reboot"                                                             # Reboot
 alias de="dolphinenable"                                                      # Execute script to fix dolphin
 alias ts="tmux-sessionizer"                                                   # Execute tmux-sessionizer script
 alias cs="~/.config/auto-commit.sh"                                           # Auto commit script
@@ -18,4 +20,4 @@ alias dolphinenable="systemctl --user start kbuildsycoca.service"             # 
 # Zsh configuration management
 alias in='nvim $(fzf -m --preview="bat --color=always {}")'            # Open file with fzf preview
 alias sudoin='sudo nvim $(fzf -m --preview="bat --color=always {}")'   # Edit file with sudo and fzf preview
-alias re="source ~/.zshrc"                                             # Reload Zsh configuration
+alias src="source ~/.zshrc"                                            # Reload Zsh configuration
